852628ddefb34c546928f4daef5eff008974609d
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2024-02-09 21:00:38 -0500

    remove unused statements

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,13 +2,11 @@ package com.example.demo;
 
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
-import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
